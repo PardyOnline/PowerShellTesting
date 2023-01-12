c9f4cc5f4e9866e7cd42d62f6808654cfa157215
PardyOnline <113913622+PardyOnline@users.noreply.github.com>
2023-01-12 17:05:58 +0000

    Demo of reading and writing to txt and csv files

---
+++ ReadAndWriteToFile.ps1
@@ -0,0 +1,23 @@
+﻿# Parenthesis Demo
+
+# Create a var to hold paths to each file
+$txtfile='C:test.txt'
+$csvfile='C:test.csv'
+
+$data = Get-Service
+
+# Write data to text file
+$data | Out-File -FilePath $txtfile
+
+# Write data to CSV file
+$data | Export-Csv -Path $csvfile -NoTypeInformation
+
+# Read data from text file
+$textData = Get-Content -Path $txtfile
+Write-Output "Data from text file:"
+Write-Output $textData
+
+# Read data from CSV file
+$csvData = Import-Csv -Path $csvfile
+Write-Output "Data from CSV file:"
+Write-Output $csvData
