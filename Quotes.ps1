0b0f8e9a8c604e5655f51ffab97b31fa2a249b35
PardyOnline <113913622+PardyOnline@users.noreply.github.com>
2023-01-12 16:08:38 +0000

    Adding Quotes Demo as it isnt visible from the first time

---
+++ Quotes.ps1
@@ -0,0 +1,16 @@
+﻿
+# Quotation Marks
+
+# Double quotes resolve the variable
+$i="PowerShell"
+
+"This is the var $i, and $i is great"
+'This is the var $i, and $i is great'
+"This is the var `$i, and $i is great"
+
+$computerName=hostname
+Get-Service -Name BITS | Select-Object @{Name='MachineName';Expression={$computerName}},
+Name, Status 
+
+$p = Get-Process lsass
+"Process ID = $($p.id)"
