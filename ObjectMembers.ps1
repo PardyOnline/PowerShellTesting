90a706bec0b3d405961ca3a724a2132790123bf4
PardyOnline <113913622+PardyOnline@users.noreply.github.com>
2023-01-12 16:19:16 +0000

    Adding demo of object members

---
+++ ObjectMembers.ps1
@@ -0,0 +1,18 @@
+﻿# Object members and variables
+# Variables are very flexible
+$Service=Get-Service -Name bits
+$Service | gm
+$Service.Status
+
+#Decide start or stop depending on status
+#$Service.Start()
+#$Service.Stop()
+$Msg="Service Name is $($service.Name.ToUpper())"
+$msg
+
+# Working with multiple objects
+$Services=Get-Service
+$services[0]
+$services[0].Status
+$Services[-1].Name
+"Service name is $($services[4].DisplayName)"
