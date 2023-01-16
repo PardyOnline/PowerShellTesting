c57643f36c6cee48f38f131329c1ce6dc0aed591
PardyOnline <113913622+PardyOnline@users.noreply.github.com>
2023-01-16 14:04:50 +0000

    2 More Demo Files for Looping and Conditional Statements

---
+++ DoWhile.ps1
@@ -0,0 +1,11 @@
+﻿$i = 1
+Do {
+    Write-Output "PowerShell Testing $i"
+    $i++ # This is important otherwise loop will run forever
+} While($i -le 5) 
+
+$i = 5
+while ($i -ge 1) {
+    Write-Output "More PowerShell Testing $i"
+    $i--
+}
---
+++ ForEach.ps1
@@ -0,0 +1,12 @@
+﻿$services = Get-Service -name b*
+ForEach($s in $services) {
+    $s.DisplayName
+}
+
+For ($i=0; $i -lt 5; $i++) {
+# Do Something
+}
+
+1..5 | ForEach-Object -process {
+    Start calc
+}
