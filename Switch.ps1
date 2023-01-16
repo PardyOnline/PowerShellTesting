059452d7242765c93f74ddf89effb885556303b4
PardyOnline <113913622+PardyOnline@users.noreply.github.com>
2023-01-16 12:24:02 +0000

    2 more demo files

---
+++ Switch.ps1
@@ -0,0 +1,8 @@
+﻿Switch ($Status) {
+    0 {$status_text = "ok" }
+    1 {$status_text = "error" }
+    2 {$status_text = "jammed" }
+    3 {$status_text = "overheated" }
+    4 {$status_text = "empty" }
+    default {$status_text = "unknown" }
+}
---
+++ if.ps1
@@ -0,0 +1,19 @@
+﻿If ($this -eq $that) {
+    # Commands
+} elseif ($those -eq $them) {
+    # Commands 
+} elseif($we -gt $they) {
+    # Commands
+} else {
+    # Commands
+}
+
+$Status=(Get-Service -Name BITS).Status
+If($Status -eq "Running") {
+    Clear-Host
+    Write-Output "Service is being stopped"
+    Stop-Service -name BITS
+} else { 
+    Clear-Host
+    Write-Output "Service is already stopped"
+}
