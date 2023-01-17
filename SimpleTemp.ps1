c69fa49b33e829061341cefd7fa673f256c23bf9
Sean Pardy <113913622+PardyOnline@users.noreply.github.com>
2023-01-17 09:30:47 +0000

    Function Demo Script

---
+++ SimpleTemp.ps1
@@ -0,0 +1,13 @@
+﻿Function Verb-Noun {
+    
+    [CmdletBinding()]
+    Param(
+        [Parameter(valuefrompipeline=$true)]
+        [int]$x
+        # [Parameter()][Int]$MyInt
+    )
+
+    Begin{<#Code#> $total = 0}
+    Process{<#Code#> $total += $x}
+    End{<#Code#> "total = $total"}
+}
